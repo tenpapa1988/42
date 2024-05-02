ee23e4552f06e14de520ac3cd749a914ec32a37d
Saitofm <Saitofm@Yusukes-MacBook-Air-2.local>
2024-05-02 09:43:32 +0900

    finished for 2nd time

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,62 +6,42 @@
 /*   By: yussaito <yussaito@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 10:17:01 by yussaito          #+#    #+#             */
-/*   Updated: 2024/04/22 07:00:02 by yussaito         ###   ########.fr       */
+/*   Updated: 2024/05/02 08:50:24 by yussaito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isspace(int c)
+int	ft_long_max(int sign)
 {
-	return (c == '\t' || c == '\n' || c == '\v' || \
-		c == '\f' || c == '\r' || c == ' ');
-}
-
-static long	ft_atoi_sub(int sign, char *str, unsigned long ulim, int lim)
-{
-	unsigned long	num;
-	int				c;
-	int				f;
-
-	f = 0;
-	num = 0;
-	while (ft_isdigit(*str))
-	{
-		c = *str++ - '0';
-		if (f < 0 || num > ulim || (num == ulim && c > lim))
-			f = -1;
-		else
-		{
-			f = 1;
-			num = (num * 10) + c;
-		}
-	}
-	if (f < 0)
-	{
-		num = LONG_MAX;
-		if (sign == -1)
-			num = LONG_MIN;
-	}
-	return (num);
+	if (sign > 0)
+		return ((int)LONG_MAX);
+	else
+		return ((int)LONG_MIN);
 }
 
 int	ft_atoi(const char *str)
 {
-	unsigned long	ulim;
-	int				lim;
-	int				sign;
+	long	result	;
+	int		sign	;
 
+	result = 0;
 	sign = 1;
-	while (ft_isspace(*str))
+	while (*str == ' ' || ('\t' <= *str && *str <= '\r'))
 		str++;
-	if (*str == '+' || *str == '-')
-		if (*(str++) == '-')
-			sign *= -1;
-	ulim = LONG_MAX;
-	if (sign == -1)
-		ulim = LONG_MIN;
-	lim = ulim % 10;
-	ulim /= 10;
-	return (ft_atoi_sub(sign, (char *)str, ulim, lim) * sign);
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while ('0' <= *str && *str <= '9')
+	{
+		if (result > (LONG_MAX - (*str - '0')) / 10)
+			return (ft_long_max(sign));
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * (int)(result));
 }
