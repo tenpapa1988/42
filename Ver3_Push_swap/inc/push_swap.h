acb169969d4bb075b4e02744101b37172d956eb8
Saitofm <Saitofm@Yusukes-MacBook-Air-2.local>
2024-09-08 11:14:40 +0900

    fixed for Linux

---
+++ Ver3_Push_swap/inc/push_swap.h
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yussaito <yussaito@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 09:07:26 by yussaito          #+#    #+#             */
+/*   Updated: 2024/09/08 08:55:05 by yussaito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <limits.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "../libft/libft.h"
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}	t_stack_node;
+
+int				ft_printf(const char *fmt, ...);
+int				error_syntax(char *str_n);
+int				error_duplicate(t_stack_node *a, int n);
+void			free_errors(t_stack_node **a);
+void			free_stack(t_stack_node	**stack);
+
+char			**split(char *str, char delimiter);
+void			free_split(char **split);
+
+void			sort_stacks(t_stack_node **a, t_stack_node **b);
+void			sort_three(t_stack_node **a);
+void			min_on_top(t_stack_node **a);
+void			rotate_both(t_stack_node **a,
+					t_stack_node **b, t_stack_node *cheapest_node);
+void			rev_rotate_both(t_stack_node **a,
+					t_stack_node **b,
+					t_stack_node *cheapest_node);
+
+void			check_argv(t_stack_node **a, char **argv);
+int				stack_len(t_stack_node *stack);
+bool			stack_sorted(t_stack_node *stack);
+t_stack_node	*find_last(t_stack_node *stack);
+t_stack_node	*find_max(t_stack_node *stack);
+t_stack_node	*find_min(t_stack_node *stack);
+
+t_stack_node	*get_cheapest(t_stack_node *stack);
+void			ready_for_push(t_stack_node **stack,
+					t_stack_node *top_node, char stack_name);
+void			current_index(t_stack_node *stack);
+void			init_nodes_a(t_stack_node *a, t_stack_node *b);
+void			init_nodes_b(t_stack_node *a, t_stack_node *b);
+
+void			pa(t_stack_node **a, t_stack_node **b, bool print);
+void			pb(t_stack_node **b, t_stack_node **a, bool print);
+void			ra(t_stack_node **a, bool print);
+void			rb(t_stack_node **b, bool print);
+void			rr(t_stack_node **a, t_stack_node **b, bool print);
+void			rra(t_stack_node **a, bool print);
+void			rrb(t_stack_node **b, bool print);
+void			rrr(t_stack_node **a, t_stack_node **b, bool print);
+void			sa(t_stack_node **a, bool print);
+void			sb(t_stack_node **b, bool print);
+void			ss(t_stack_node **a, t_stack_node **b, bool print);
+
+#endif
---
+++ Ver3_Push_swap/libft/ft_lstdelone.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yussaito <yussaito@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 13:16:47 by yussaito          #+#    #+#             */
+/*   Updated: 2024/04/23 15:48:46 by yussaito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (lst == NULL || del == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+}
