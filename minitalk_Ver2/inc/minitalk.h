3ce0451750380016a53c2db4c05f61efe08bf186
Yusuke Saito <yussaito@c5r1s6.42tokyo.jp>
2024-10-20 10:12:09 +0900

    Removed .git directory and added minitalk_Ver2 as a regular directory

---
+++ minitalk_Ver2/inc/minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yussaito <yussaito@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 09:24:14 by yussaito          #+#    #+#             */
+/*   Updated: 2024/10/12 13:31:17 by yussaito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# define INPUT_ERROR 1
+# define PID_ERROR 2
+# define KILL_ERROR 3
+# define RESPONSE_ERROR 4
+
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "../libft/ft_printf.h"
+
+#endif
