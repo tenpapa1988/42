bf74bceed80fc6a0220d03a2ba195a030027ef7e
Yusuke Saito <yussaito@c5r1s8.42tokyo.jp>
2024-10-26 14:59:44 +0900

    updated so_long_Ver1

---
+++ so_long_Ver1/src/hook_functions.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hook_functions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yussaito <yussaito@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 14:30:56 by yussaito          #+#    #+#             */
+/*   Updated: 2024/10/26 14:45:24 by yussaito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+
+static void	set_player_position(t_game *game, size_t x, size_t y)
+{
+	game->player.x = x;
+	game->player.y = y;
+}
+int	ft_input(int key, void *game_)
+{
+	size_t	x;
+	size_t	y;
+	t_game	*game;
+
+	game = (t_game *)game_;
+	x = game->player.x;
+	y = game->player.y;
+	if (key == ESC)
+		mlx_loop_end(game->mlx);
+	if (key == W)
+		game->player.y -= 1;
+	else if (key == S)
+		game->player.y += 1;
+	else if (key == A)
+		game->player.x -= 1;
+	else if (key == D)
+		game->player.x += 1;
+	else
+		return (0);
+	if (can_move(game, game->map.map[game->player.y][game->player.x]))
+		ft_update(game, x, y);
+	else
+		set_player_position(game, x, y);
+	return (0);
+}
