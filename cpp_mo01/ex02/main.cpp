95f850446ccd7065b4501a3e7d81e78507a6235d
Yusuke Max Ubuntu <yusuke.mac.ubuntu>
2025-04-16 23:03:49 +0000

    finished ex02

---
+++ cpp_mo01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yussaito <yussaito@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 22:23:14 by yussaito          #+#    #+#             */
+/*   Updated: 2025/04/16 22:52:13 by yussaito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main() {
+	std::string str = "HI THIS IS BRAIN";
+
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+    std::cout << "Memory address of str:        " << &str << std::endl;
+    std::cout << "Memory address held by PTR:   " << stringPTR << std::endl;
+    std::cout << "Memory address held by REF:   " << &stringREF << std::endl;
+
+	std::cout << "Value of str:                 " << str << std::endl;
+    std::cout << "Value pointed to by PTR:      " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by REF:      " << stringREF << std::endl;
+
+	return 0;
+}
