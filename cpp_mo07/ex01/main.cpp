3b1e1f59545b1763fc8b07db8d872b617b08ec84
tenpapa1988 <tenpapa1988@gmail.com>
2025-07-01 11:52:24 +0900

    finished ex01

--- cpp_mo07/ex01/main.cpp
+++ cpp_mo07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yussaito <yussaito@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 11:26:00 by yussaito          #+#    #+#             */
-/*   Updated: 2025/07/01 11:26:25 by yussaito         ###   ########.fr       */
+/*   Updated: 2025/07/01 11:50:50 by yussaito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,11 @@ void increment(int& x) {
 
 int main() {
     int intArr[5] = {1, 2, 3, 4, 5};
-    ::iter(intArr, 5, increment);
-    ::iter(intArr, 5, print);
+    iter(intArr, 5, increment);
+    iter(intArr, 5, print<int>);
 
     const std::string strArr[3] = {"hello", "world", "cpp"};
-    ::iter(strArr, 3, printConst);
+    iter(strArr, 3, printConst<std::string>); 
 
     return 0;
 }
