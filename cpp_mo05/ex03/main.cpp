773c771b7e0e0d41d4d2702b668d1e8e7e128ab3
tenpapa1988 <tenpapa1988@gmail.com>
2025-07-25 08:32:08 +0900

    fixed main

--- cpp_mo05/ex03/main.cpp
+++ cpp_mo05/ex03/main.cpp
@@ -6,27 +6,9 @@
 /*   By: yussaito <yussaito@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 10:15:11 by yussaito          #+#    #+#             */
-/*   Updated: 2025/07/23 12:47:34 by yussaito         ###   ########.fr       */
+/*   Updated: 2025/07/25 08:17:48 by yussaito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include "Intern.hpp"
-// #include <iostream>
-
-// int main() {
-//     Intern someRandomIntern;
-//     AForm* form;
-
-//     form = someRandomIntern.makeForm("robotomy request", "Bender");
-//     if (form)
-//         delete form;
-
-//     form = someRandomIntern.makeForm("unknown form", "Bender");
-//     if (form)
-//         delete form;
-
-//     return 0;
-// }
 
 #include "Intern.hpp"
 #include "Bureaucrat.hpp"
