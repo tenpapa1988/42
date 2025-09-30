db4ad9df7fc1c091c86814ba9861225265fbd59b
yusuke <Saitofm@Yusukes-MacBook-Air-2.local>
2025-10-01 07:12:15 +0900

    Convert urabe_inception from submodule to regular directory

---
+++ urabe_inception/srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# エラー発生時に即終了させる
+set -e
+
+# SQLファイルを動的生成(PHPで記述するとENVを使用できないため)
+cat << EOF > /docker-entrypoint-initdb.d/init.sql
+
+SELECT 'Initializing database...' AS message;
+
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+
+SELECT 'Database initialized.' AS message;
+
+EOF
+
+# 生成したSQLを使って初期化しながらMariaDBを起動させる
+exec mariadbd --init-file=/docker-entrypoint-initdb.d/init.sql
---
+++ urabe_inception/srcs/requirements/nginx/tools/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# エラー発生時に即終了させる
+set -e
+
+# SSL証明書の保存先移動
+cd /etc/nginx/ssl
+
+# 秘密鍵作成(NISTなどのセキュリティ機関が2048bit以上を推奨しており、パフォーマンスバランスがいいためこちらを指定)
+openssl genrsa -out inception.key 2048
+
+# CSR(証明書署名要求)作成
+openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=yussaito.42.fr"
+
+# CSRから自己署名証明書を作成
+openssl x509 -req -days 365 -in inception.csr -signkey inception.key -out inception.crt
+
+# Nginx実行
+exec nginx -g 'daemon off;'
